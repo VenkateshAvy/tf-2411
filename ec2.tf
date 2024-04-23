f70db8fe66e2e09c2d155823c371712a4b300a10
venkateshangirekula <venkateshangirekula27@gmail.com>
2024-04-23 15:56:05 +0530

    chnged memory

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "lms-web-server" {
   ami           = "ami-010b74bc1a8b29122"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   subnet_id = aws_subnet.lms-web-subnet.id
   key_name = "sunnystockholm"
   vpc_security_group_ids = [aws_security_group.lms-web-sg.id]

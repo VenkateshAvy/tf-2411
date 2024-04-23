fdb1ea916b13823933d34ea172b65aec08c4a8ab
venkateshangirekula <venkateshangirekula27@gmail.com>
2024-04-23 15:06:04 +0530

    creates db sg

--- vpc.tf
+++ vpc.tf
@@ -229,3 +229,37 @@ resource "aws_vpc_security_group_egress_rule" "lms-api-sg-egress" {
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
+
+#db security group
+
+resource "aws_security_group" "lms-db-sg" {
+  name        = "lms-db-sg"
+  description = "Allow SSH & HTTP Traffic"
+  vpc_id      = aws_vpc.lms-vpc.id
+
+  tags = {
+    Name = "lms-db-sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "lms-db-sg-ingress-ssh" {
+  security_group_id = aws_security_group.lms-db-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "lms-db-sg-ingress-postgres" {
+  security_group_id = aws_security_group.lms-db-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 5432
+  ip_protocol       = "tcp"
+  to_port           = 5432
+}
+
+resource "aws_vpc_security_group_egress_rule" "lms-db-sg-egress" {
+  security_group_id = aws_security_group.lms-db-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}

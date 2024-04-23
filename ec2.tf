205b835da8d192b70c70d95fa50ff305c159226b
venkateshangirekula <venkateshangirekula27@gmail.com>
2024-04-23 15:47:05 +0530

    added braces

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web-server" {
   instance_type = "t2.micro"
   subnet_id = aws_subnet.lms-web-subnet.id
   key_name = "sunnystockholm"
-  vpc_security_group_ids = aws_security_group.lms-web-sg.id
+  vpc_security_group_ids = [aws_security_group.lms-web-sg.id]
   user_data = file("setup.sh")
   tags = {
     Name = "web-server"

d20d11019a16ea5c4338f92599d286109385339b
venkateshangirekula <venkateshangirekula27@gmail.com>
2024-04-23 11:38:21 +0530

    created subnets

--- vpc.tf
+++ vpc.tf
@@ -6,3 +6,27 @@ resource "aws_vpc" "lms-vpc" {
     Name = "lms-vpc"
   }
 }
+resource "aws_subnet" "lms-web-subnet" {
+  vpc_id     = aws_vpc.lms-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "lms-web-subnet"
+  }
+}
+resource "aws_subnet" "lms-api-subnet" {
+  vpc_id     = aws_vpc.lms-vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "lms-api-subnet"
+  }
+}
+resource "aws_subnet" "lms-db-subnet" {
+  vpc_id     = aws_vpc.lms-vpc.id
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "lms-db-subnet"
+  }
+}
